42b4f773ab9e43dc4f1485d3fbb973cfe4bbf6c7
C4Jay <choolakejay@gmail.com>
2019-12-26 10:15:57 +0530

    Menu Working

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,47 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app dark>
+    
+    <v-toolbar dark fixed app>
+      <v-app-bar-nav-icon @click="Hamburg = !Hamburg"></v-app-bar-nav-icon>
+        <v-toolbar-title>MeetUps</v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-toolbar-items class="hidden-sm-and-down">
+          <v-btn>MeetUps </v-btn>
+          <v-btn>SignIn</v-btn>
+           <v-btn>Join</v-btn>
+       
+        </v-toolbar-items>
+        
+      </v-toolbar>
+<v-navigation-drawer color="purple" v-model="Hamburg">
+      <v-list>
+        <v-list-tile>
+          <v-list-tile-action>
+          </v-list-tile-action>
+            <v-list-tile-content>View Meetups</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+<main>
+
+
+   </main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-#nav {
-  padding: 30px;
-}
+import 'vue-material-design-icons/styles.css';
+export default {
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  name: 'App',
+  data() {
+    return {
+      Hamburg: false
+    }
+  },
+  components: {
+ 
+  }
+};
+</script>
