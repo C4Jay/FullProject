6716231fe192c7598106be86d8dc560ef427f0e1
C4Jay <choolakejay@gmail.com>
2019-12-27 11:34:20 +0530

    begining-card

--- src/components/ViewMeetups.vue
+++ src/components/ViewMeetups.vue
@@ -1,9 +1,28 @@
 <template>
-<div>
-<p>
-    hey there
-</p>
-</div>
+<v-container>
+    <v-layout>
+        <v-flex xs12>
+            <v-card class="info">
+                <v-container fluid>
+                    <v-layout row>
+                        <v-flex xs5>
+                            <div class="card">
+                            <v-img src="https://i.ytimg.com/vi/gYLQThUxpvQ/maxresdefault.jpg"></v-img>
+                            </div>
+                        </v-flex>
+                        <v-flex xs10 sm8 md7>
+                            <v-card-title primary-title>
+                                <h3 class="white--text">New York MeetUp</h3>
+                                <div>Our New York meetup was held on 8th lasth month</div>
+                            </v-card-title>
+                        </v-flex>
+                    </v-layout>
+                </v-container>
+
+            </v-card>
+        </v-flex>
+    </v-layout>
+</v-container>
 </template>
 
 
@@ -12,3 +31,10 @@ export default {
     name: 'ViewMeetups'
 }
 </script>
+
+<style scoped>
+.card {
+    position: relative;
+    left: 8px;
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,11 @@
 <template>
   <v-container grid-list-lg>
     <v-layout row wrap>
-      <v-flex sm12 md6 class="text-sm-center text-md-right">
+      <v-flex sm12 md6 class="text-center text-sm-right">
         <v-btn color="yellow" large to="/meetups">Explore Meetups</v-btn>
         
       </v-flex>
-      <v-flex sm12 md6 class="text-sm-center text-md-left">
+      <v-flex sm12 md6 class="text-center text-sm-left">
       
         <v-btn color="green" large to="/meetups/new">Organize Meetups</v-btn>
       
@@ -22,8 +22,20 @@
           v-for="item in items"
           v-bind:src="item.img"
           :key="item.title">
+          <v-row justify="center">
+          <div class="title">
+            {{ item.title }}
+          </div>
+          </v-row>
           </v-carousel-item>
         </v-carousel>
+        <v-layout class="mt-2" row wrap>
+          
+          <v-flex xs12 class="text-center">
+          <p>Join Our Meetups</p>
+          </v-flex>
+        </v-layout>
+    
       </v-flex>
     </v-layout>
 
@@ -54,3 +66,15 @@ data() {
   }
 }
 </script>
+
+<style scoped>
+.title {
+  
+  position: absolute;
+  bottom: 55px;
+  color:ghostwhite;
+  font-size: 2em;
+  padding: 10px;
+  background-color: rgba(0, 0, 0, 0.5)
+}
+</style>
