f9f79249da26e7d9da6ad91d6bc63482f75a4c2f
C4Jay <choolakejay@gmail.com>
2019-12-28 11:19:10 +0530

    Signup-created

--- src/components/SignUp.vue
+++ src/components/SignUp.vue
@@ -1,12 +1,108 @@
 <template>
-    <p>
-        SignUp
-    </p>
+<v-container>
+<v-layout row> 
+    <v-flex xs12 sm6 offset-sm3>
+        <v-card>
+            <v-card-text>
+                <v-container>
+                    <v-form>
+                        <v-layout row>
+                            <v-flex xs12 sm6 offeset-sm3>
+                                <v-text-field
+                                    name="mail"
+                                    label="Mail"
+                                    id="mail"
+                                    v-model="mail"
+                                    type="email"
+                                    required
+                                    >
+
+                                </v-text-field>
+                            </v-flex>
+
+                        </v-layout>
+                        <v-layout row>
+                            <v-flex xs12 sm6 offeset-sm3>
+                                <v-text-field
+                                    name="password"
+                                    label="Password"
+                                    id="password"
+                                    v-model="password"
+                                    type="password"
+                                    required
+                                    >
+
+                                </v-text-field>
+                            </v-flex>
+
+                        </v-layout>
+
+                        <v-layout row>
+                            <v-flex xs12 sm6 offeset-sm3>
+                                <v-text-field
+                                    name="confirmPassword"
+                                    label="Confirm Password"
+                                    id="confirmPassword"
+                                    v-model="confirmPassword"
+                                    type="password"
+                                    :rules="[comparePasswords]"
+                                   
+                                    >
+
+                                </v-text-field>
+                            </v-flex>
+
+                        </v-layout>
+
+                        <v-layout row>
+                            <v-flex xs12>
+                                <v-btn color="yellow" type="submit">
+                                    SignUp</v-btn>
+
+                            </v-flex>
+                        </v-layout>
+
+
+
+                    </v-form>
+
+
+
+                </v-container>
+
+            </v-card-text>
+
+        </v-card>
+
+    </v-flex>
+
+</v-layout>
+</v-container>
+
 </template>
 
 <script>
 export default {
-    name: 'SignUp'
+    name: 'SignUp',
+
+    data () {
+        return {
+            mail: '',
+            password: '',
+            confirmPassword: ''
+        }
+    },
+    computed: {
+        comparePasswords () {
+            return this.password !== this.confirmPassword ? 'Passwords do not match' : ''
+        }
+
+    },
+    methods: {
+        onSignup () {
+
+        }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify';
+import * as firebase from 'firebase';
 import Filtering from './filters/filter';
 import 'vue-material-design-icons/styles.css';
 import 'material-design-icons-iconfont/dist/material-design-icons.css'
@@ -16,5 +17,17 @@ new Vue({
   router,
   store,
   vuetify,
-  render: h => h(App)
+  render: h => h(App),
+  created () {
+    firebase.initializeApp ({
+    apiKey: "AIzaSyAGPRd8myZqAo91ow28TAyFV5cdH8PPwQ8",
+    authDomain: "meetups-97bdb.firebaseapp.com",
+    databaseURL: "https://meetups-97bdb.firebaseio.com",
+    projectId: "meetups-97bdb",
+    storageBucket: "meetups-97bdb.appspot.com",
+    messagingSenderId: "295730922301",
+    appId: "1:295730922301:web:4abcd0fb7dfaa640b5bb54"
+    })
+  }
+
 }).$mount('#app')
